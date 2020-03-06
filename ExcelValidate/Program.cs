7418a30d8e7e10321ec34d38139bd8c982a83e59
rajesh-zero <rajesh_zero@outlook.com>
2020-03-06 22:57:49 +0530

    valaditing column data for empty or null values as well as checking dateof birth column and Isactive column

--- ExcelValidate/Program.cs
+++ ExcelValidate/Program.cs
@@ -11,31 +11,40 @@ namespace ExcelValidate
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            //Excel file location
-            var fi = new FileInfo(@"C:\Users\Rajesh\source\repos\ExcelValidate\ExcelValidate\ExcelData.xlsx");
+            //Excel file location 
+            var fi = new FileInfo(@"..\..\..\ExcelData.xlsx"); //relative path for excel file
             //Creating Exccel file package that will contain ExcelData.xlsx
             ExcelPackage excel = new ExcelPackage(fi);
             //Specifying sheet name
             ExcelWorksheet firstWorksheet = excel.Workbook.Worksheets["Sheet1"];
             //Creating for loop to print all the rows available in Sheet1
             //firstWorksheet.Dimension.End.Row gets the last row number that contains data
-            for (int i = 1; i <= firstWorksheet.Dimension.End.Row; i++)
+            for (int i = 2; i <= firstWorksheet.Dimension.End.Row; i++)
             {
-                for (int j = 1; j <= 5; j++)
+                var Name = firstWorksheet.Cells[i, 1].Value;
+                DateTime DateOfBirth;
+                bool IsActive = (bool)firstWorksheet.Cells[i, 3].Value;
+                var Balance = firstWorksheet.Cells[i, 4].Value;
+                var LoanAmount = firstWorksheet.Cells[i, 5].Value;
+
+                Console.WriteLine("results");
+                Console.WriteLine(Name != null);
+                Console.WriteLine(DateTime.TryParse(firstWorksheet.Cells[i, 2].Value.ToString(), out DateOfBirth) == true);
+                Console.WriteLine(IsActive is true);
+                Console.WriteLine(Balance != null);
+                Console.WriteLine(LoanAmount != null);
+                //Console.WriteLine(Name != null && DateTime.TryParse(firstWorksheet.Cells[i, 2].Value.ToString(), out DateOfBirth) == true && IsActive.ToString() == "true" && Balance != null && LoanAmount != null);
+                if( Name != null 
+                    && DateTime.TryParse(firstWorksheet.Cells[i, 2].Value.ToString(), out DateOfBirth) == true
+                    && IsActive is true
+                    && Balance != null 
+                    && LoanAmount != null)
                 {
-                    Console.Write(firstWorksheet.Cells[i, j].Value + " ");
-                    //taking name column value in namevar
-                    var namevar = firstWorksheet.Cells[i, j].Value;
-                    if (j == 1 && namevar != null && i > 1)//if name is not null then
-                    {
-                        
-                        apiRequest(firstWorksheet.Cells[i, j].Value.ToString()); //call apiRequestmethod
-                        firstWorksheet.Cells[i, 6].Value = "Success"; //update success at row end
-                    }
-                    else if(j == 1 && namevar == null && i > 1)//if name is null then
-                    {
-                        firstWorksheet.Cells[i, 6].Value = "failure"; //update failure at row end
-                    }
+                    Console.WriteLine(Name.ToString()
+                        + DateOfBirth.ToString()
+                        + IsActive.ToString()
+                        + Balance.ToString()
+                        + LoanAmount.ToString());
                 }
                 Console.WriteLine("\n");
             }
